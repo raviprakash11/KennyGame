dc0fe24df4a191f22288a7c27497ff65ff7a3aa0
Ravi Prakash <ravi_201600714@smit.smu.edu.in>
2022-07-06 08:27:02 +0530

    initial commit

---
+++ app/src/main/java/com/raviprakash11/kennygame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raviprakash11.kennygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
